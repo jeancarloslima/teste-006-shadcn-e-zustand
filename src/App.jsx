efa5bfda75a39be60dfb760ef80b39405ec77adf
Jean Lima <jcoliveiralima19@gmail.com>
2026-04-28 13:45:19 -0300

    Importando botão com shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Tela01 from "./components/Tela01";
+import Tela02 from "./components/Tela02";
 
 function App() {
-  return <>
-  <Tela01 />
-  </>;
+  return (
+    <>
+      {/* <Tela01 /> */}
+      <Tela02 />
+    </>
+  );
 }
 
 export default App;
